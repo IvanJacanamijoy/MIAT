e1dcde1b2aa14ac79ac767ea2dc05fa9498ac4e9
Ivan Yesid Rojas Jacanamijoy <rojasjacanamijoyivan@gmail.com>
2025-04-16 23:19:07 -0500

    corriguiendo errores de responsividad

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -6,12 +6,12 @@ import Navbar from '../components/Navbar'
 const MainLayout = () => {
     return (
         <div className="app">
-            <Navbar className="absolute top-0 left-0 right-0 z-20"/>
-            <main className='flex-grow relative'>
+            <Navbar />
+            <main >
                 {/* aqui se renderizan las paginas */}
                 <Outlet />
             </main>
-            <Footer className="z-20"/>
+            <Footer/>
         </div>
     )
 }
