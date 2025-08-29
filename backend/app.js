cfb2ceb63d74b94069fe2c1ffd3c3620697fdfaf
jottamoral <moralesjmd2002@gmail.com>
2025-08-28 21:30:34 -0500

    Creación del archivo QuoteRoutes.js

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,9 @@ const authRouter = require('./routes/authRoutes');
 const servicioRouter = require('./routes/servicioRoutes');
 //importamos las rutas de visitas tecnicas
 const visitaTecnicaRouter = require('./routes/VisitaTecnicaRoutes')
+//importamos las rutas de cotizaciones
+const quoteRouter = require('./routes/QuoteRoutes');
+
 
 //inicializamos knex
 const knex = require('knex')(require('./config/knexfile').development);
@@ -32,6 +35,8 @@ app.use('/usuarios', usuarioRouter);
 app.use('/servicios', servicioRouter);
 // Rutas de visitas tecnicas
 app.use('/visitatecnica', visitaTecnicaRouter);
+// Rutas de cotizaciones
+app.use('/cotizaciones', quoteRouter);
 
 // Probando ruta de prueba
 app.use('/prueba', (req, res) => {
