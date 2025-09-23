075b8808f0a33844188cdce7d956e68dd75484db
Ivan Yesid Rojas Jacanamijoy <156599191+IvanJacanamijoy@users.noreply.github.com>
2025-09-23 16:58:15 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://miat.site',
+  credentials: true
+}));
 app.use(express.json());
 
 // Servir archivos estáticos desde la carpeta uploads
